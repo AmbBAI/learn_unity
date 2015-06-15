160880cf11759dfe650f5f067c6faac0e072124a
AmbBAI <amb.main@gmail.com>
2015-06-15 16:22:37 +0800

    test nav mesh

---
+++ Assets/_Script/NavMeshTest.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NavMeshTest : MonoBehaviour {
+
+	public NavMeshAgent agent;
+	public Animation anim;
+
+	void Update () {
+
+		if (!agent.hasPath) anim.CrossFade("idle");
+		if (Input.GetMouseButtonDown(0))
+		{
+			Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+			RaycastHit hitInfo;
+			if(Physics.Raycast(ray, out hitInfo))
+			{
+				if (agent.SetDestination(hitInfo.point))
+				{
+					anim.CrossFade("run");
+				}
+			}
+		}
+	}
+}
