7341c3142423fd88f1a4a90364197ef9edf7051b
AmbBAI <amb.main@gmail.com>
2015-07-06 19:32:11 +0800

    batch

--- Assets/_Script/BatchTest.cs
+++ Assets/_Script/BatchTest.cs
@@ -7,12 +7,14 @@ public class BatchTest : MonoBehaviour {
 	public GameObject obj;
 	List<GameObject> objPool = new List<GameObject>();
 	int vertexCount = 0;
+	int triangleCount = 0;
 
 	void Start()
 	{
 		MeshFilter mf = obj.GetComponent<MeshFilter>();
 		Mesh m = mf.sharedMesh;
 		vertexCount = m.vertexCount;
+		triangleCount = m.triangles.Length / 3;
 	}
 
 	void OnGUI()
@@ -20,9 +22,13 @@ public class BatchTest : MonoBehaviour {
 		Rect rect = new Rect(0, 0, 200, 20);
 		GUI.Label(rect, "Mesh Vertex Count: " + vertexCount);
 		rect.Set(rect.xMin, rect.yMin + 20, rect.width, rect.height);
+		GUI.Label(rect, "Mesh Triangle Count: " + triangleCount);
+		rect.Set(rect.xMin, rect.yMin + 20, rect.width, rect.height);
 		GUI.Label(rect, "Mesh Count: " + objPool.Count);
 		rect.Set(rect.xMin, rect.yMin + 20, rect.width, rect.height);
 		GUI.Label(rect, "Total Vertex Count: " + vertexCount * objPool.Count);
+		rect.Set(rect.xMin, rect.yMin + 20, rect.width, rect.height);
+		GUI.Label(rect, "Total Triangle Count: " + triangleCount * objPool.Count);
 		rect.Set(rect.xMin, rect.yMin + 20, rect.width, rect.height);
 		if (GUI.Button(rect, "Add Obj"))
 		{
