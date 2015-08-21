07069404f6e974cb97165d4729bf515270afea4a
AmbBAI <amb.main@gmail.com>
2015-08-21 18:48:24 +0800

    ui

---
+++ Assets/_Script/UI/UITest.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class UITest : MonoBehaviour {
+
+	public Text sliderText;
+
+	public void ButtonClickCallback()
+	{
+		
+	}
+
+	public void SliderValueChangedCallback(Slider slider)
+	{
+		sliderText.text = slider.value.ToString();
+	}
+}
