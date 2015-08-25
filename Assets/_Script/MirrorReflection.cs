21c92a8ed48a4912d77be090c8a48c90d68ec62e
AmbBAI <amb.main@gmail.com>
2015-08-25 18:57:28 +0800

    ?

--- Assets/_Script/MirrorReflection.cs
+++ Assets/_Script/MirrorReflection.cs
@@ -2,7 +2,6 @@
 using System.Collections;
 using System.Collections.Generic;
 
-[ExecuteInEditMode]
 public class MirrorReflection : MonoBehaviour
 {
 	public Camera mainCamera;
@@ -11,7 +10,8 @@ public class MirrorReflection : MonoBehaviour
 	public float clipPlaneOffset = 0.07f;
 
 	private static int renderDepth = 0;
-	public const int renderDepthMax = 2;
+	private static MirrorReflection lastMirror = null;
+	public const int renderDepthMax = 1;
 
 	class MirrorRender
 	{
@@ -45,23 +45,26 @@ public class MirrorReflection : MonoBehaviour
 		if (!currentCamera) return;
 		if (currentCamera == mainCamera) renderDepth = 0;
 
-		if (renderDepth >= renderDepthMax) return;
+		if (renderDepth >= renderDepthMax)
+		{
+			SetReflectTexture(null);
+			return;
+		}
 
 		CreateMirrorObjects(currentCamera, ref mirrorRenderers[renderDepth]);
 		Camera reflectionCamera = mirrorRenderers[renderDepth].camera;
 
-		Vector3 pos = transform.position;
+		Vector3 position = transform.position;
 		Vector3 normal = transform.up;
-		float d = -Vector3.Dot(normal, pos) - clipPlaneOffset;
+		float d = -Vector3.Dot(normal, position) - clipPlaneOffset;
 		Vector4 reflectionPlane = new Vector4(normal.x, normal.y, normal.z, d);
 
 		Matrix4x4 reflection = Matrix4x4.zero;
 		CalculateReflectionMatrix(ref reflection, reflectionPlane);
-		Vector3 oldpos = currentCamera.transform.position;
-		Vector3 newpos = reflection.MultiplyPoint(oldpos);
+		//reflectionCamera.transform.position = reflection.MultiplyPoint(currentCamera.transform.position);
 		reflectionCamera.worldToCameraMatrix = currentCamera.worldToCameraMatrix * reflection;
 
-		Vector4 clipPlane = CameraSpacePlane(reflectionCamera, pos, normal, 1.0f);
+		Vector4 clipPlane = CameraSpacePlane(reflectionCamera, position, normal, 1.0f);
 		Matrix4x4 projection = currentCamera.CalculateObliqueMatrix(clipPlane);
 		reflectionCamera.projectionMatrix = projection;
 
@@ -69,16 +72,20 @@ public class MirrorReflection : MonoBehaviour
 
 		renderDepth += 1;
 		GL.SetRevertBackfacing(true);
-		reflectionCamera.transform.position = newpos;
-		Vector3 euler = currentCamera.transform.eulerAngles;
-		reflectionCamera.transform.eulerAngles = new Vector3(0, euler.y, euler.z);
-		//Debug.Log("<" + GetInstanceID() + "," + renderDepth);
 		reflectionCamera.Render();
-		//Debug.Log(GetInstanceID() + "," + renderDepth + ">");
 		GL.SetRevertBackfacing(false);
 		renderDepth -= 1;
 
 		SetReflectTexture(mirrorRenderers[renderDepth].target);
+
+	}
+
+	void OnRenderObject()
+	{
+		if (mirrorRenderers.Length > 0)
+		{
+			SetReflectTexture(mirrorRenderers[0].target);
+		}
 	}
 
 
@@ -138,16 +145,12 @@ public class MirrorReflection : MonoBehaviour
 		if (reflectionRender.camera != null)
 		{
 			reflectionRender.camera.enabled = false;
-			reflectionRender.camera.transform.position = transform.position;
-			reflectionRender.camera.transform.rotation = transform.rotation;
 		}
 		else
 		{
 			GameObject go = new GameObject(GetInstanceID() + "," + renderDepth, typeof(Camera), typeof(Skybox));
 			reflectionRender.camera = go.camera;
 			reflectionRender.camera.enabled = false;
-			reflectionRender.camera.transform.position = transform.position;
-			reflectionRender.camera.transform.rotation = transform.rotation;
 			reflectionRender.camera.gameObject.AddComponent("FlareLayer");
 			go.hideFlags = HideFlags.DontSave;
 		}
@@ -155,25 +158,15 @@ public class MirrorReflection : MonoBehaviour
 		UpdateCameraModes(currentCamera, reflectionRender.camera);
 	}
 
-	// Extended sign: returns -1, 0 or 1 based on sign of a
-	private static float sgn(float a)
+	private Vector4 CameraSpacePlane(Camera cam, Vector3 position, Vector3 normal, float sideSign)
 	{
-		if (a > 0.0f) return 1.0f;
-		if (a < 0.0f) return -1.0f;
-		return 0.0f;
-	}
-
-	// Given position/normal of the plane, calculates plane in camera space.
-	private Vector4 CameraSpacePlane(Camera cam, Vector3 pos, Vector3 normal, float sideSign)
-	{
-		Vector3 offsetPos = pos + normal * clipPlaneOffset;
+		Vector3 offsetPos = position + normal * clipPlaneOffset;
 		Matrix4x4 m = cam.worldToCameraMatrix;
 		Vector3 cpos = m.MultiplyPoint(offsetPos);
 		Vector3 cnormal = m.MultiplyVector(normal).normalized * sideSign;
 		return new Vector4(cnormal.x, cnormal.y, cnormal.z, -Vector3.Dot(cpos, cnormal));
 	}
 
-	// Calculates reflection matrix around the given plane
 	private static void CalculateReflectionMatrix(ref Matrix4x4 reflectionMat, Vector4 plane)
 	{
 		reflectionMat.m00 = (1F - 2F * plane[0] * plane[0]);
