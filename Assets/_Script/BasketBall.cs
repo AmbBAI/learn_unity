11bb320c4ca2d7c8f7d13014b7e9983c90d5202a
AmbBAI <amb.main@gmail.com>
2015-08-31 16:13:47 +0800

    physic test

---
+++ Assets/_Script/BasketBall.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BasketBall : MonoBehaviour {
+
+	Rigidbody _rigidbody;
+	public Transform targetTrans;
+	Transform _transform;
+
+	Vector3 virtualTarget;
+
+	void Start () {
+		_transform = transform;
+		_rigidbody = GetComponent<Rigidbody>();
+	}
+
+	void Update()
+	{
+		if (_transform.position.y < -1f)
+		{
+			_rigidbody.isKinematic = true;
+			_transform.position = Vector3.zero;
+			_transform.rotation = Quaternion.identity;
+		}
+	}
+
+	[ContextMenu("Shot")]
+	public void Shot()
+	{
+		Vector3 velocity = CalcThrowVelocity();
+		Debug.Log(velocity);
+		_rigidbody.isKinematic = false;
+		_rigidbody.velocity = velocity;
+	}
+
+	Vector3 CalcThrowVelocity()
+	{
+		Vector3 delta = targetTrans.position - _transform.position;
+		virtualTarget = targetTrans.position - delta * 0.1f;
+		virtualTarget.y = 2f * targetTrans.position.y - virtualTarget.y;
+		Debug.Log(virtualTarget);
+		delta = virtualTarget - _transform.position;
+		float t = Random.Range(0.9f, 1f);
+		return (delta - Physics.gravity * (0.5f * t * t)) / t;
+	}
+
+	void OnDrawGizmos()
+	{
+		Gizmos.DrawSphere(virtualTarget, 0.2f);
+	}
+}
---
+++ Assets/_Script/RigidbodyTest.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(Rigidbody))]
+public class RigidbodyTest : MonoBehaviour {
+
+	Rigidbody _rigidbody;
+	public Vector3 force;
+	public Vector3 velocity;
+
+	// Use this for initialization
+	void Start () {
+		_rigidbody = GetComponent<Rigidbody>();
+	}
+
+	[ContextMenu("AddForce")]
+	public void AddForce()
+	{
+		_rigidbody.AddForce(force);
+	}
+
+	[ContextMenu("AddVelocity")]
+	public void AddVelocity()
+	{
+		_rigidbody.velocity += velocity;
+	}
+}
