a73509da20fa19f4d1f5f382bd79b4e1e4e04b57
AmbBAI <amb.main@gmail.com>
2015-11-01 16:52:37 +0800

    fmod

---
+++ Assets/_Script/FmodCarEngine.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+using FMOD.Studio;
+
+public class FmodCarEngine : MonoBehaviour {
+
+	public FMOD_StudioEventEmitter emitter;
+	ParameterInstance rpmParameter;
+	ParameterInstance loadParameter;
+	[Range(0f, 10000f)]
+	public float rpm;
+	[Range(-1f, 1f)]
+	public float load;
+
+	void Update()
+	{
+		if (rpmParameter == null)
+		{
+			rpmParameter = emitter.getParameter("RPM");
+			loadParameter = emitter.getParameter("Load");
+		}
+
+		rpmParameter.setValue(rpm);
+		loadParameter.setValue(load);
+	}
+}
