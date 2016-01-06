50ccbcb2809e3925098db4b914c31d49c3739098
AmbBAI <amb.main@gmail.com>
2016-01-06 18:33:45 +0800

    test foreach

--- Assets/_Script/CSTest.cs
+++ Assets/_Script/CSTest.cs
@@ -21,6 +21,81 @@ public class CSTest : MonoBehaviour {
     Rect rect1 = new Rect(0, 0, 500, 20);
     Rect rect2 = new Rect(0, 20, 500, 20);
 
+
+	List<int> list = new List<int>() { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+
+	int TestFor()
+	{
+		int ret = 0;
+		for (int i=0; i<list.Count; ++i)
+		{
+			ret += list[i];
+		}
+		return ret;
+	}
+
+	int TestForEach()
+	{
+		int ret = 0;
+		foreach (var i in list)
+		{
+			ret += i;
+		}
+		return ret;
+	}
+
+	int TestListForEach()
+	{
+		int ret = 0;
+		list.ForEach(i => ret += i);
+		return ret;
+	}
+
+	int TestWhile()
+	{
+		int ret = 0;
+		var e = list.GetEnumerator();
+		while (e.MoveNext())
+		{
+			ret += e.Current;
+		}
+		return ret;
+	}
+
+	int TestWhileUsing()
+	{
+		int ret = 0;
+		using (var e = list.GetEnumerator())
+		{
+			while (e.MoveNext())
+			{
+				ret += e.Current;
+			}
+		}
+		return ret;
+	}
+
+	int TestForDelegate()
+	{
+		int ret = 0;
+		Action<int> func = x => ret += x;
+		for (int i=0; i< list.Count; ++i)
+		{
+			func(list[i]);
+		}
+		return ret;
+	}
+
+	void Update()
+	{
+		TestFor();
+		TestForEach();
+		TestListForEach();
+		TestWhile();
+		TestWhileUsing();
+		TestForDelegate();
+	}
+
     void OnGUI()
 	{
 		GUI.Label(rect1, add(1, 2).ToString());
